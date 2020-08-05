d9afb8109912f2c0561ddee7d2ceee02e208ee0e
Ryan Patrick Kyle <ryan@plot.ly>
2020-08-05 01:07:32 -0400

    document dataset for CRAN

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Tips received by a single server over a two month period
+#'
+#' In 1990, a server recorded data on all tips received
+#' during a two and a half month period working in a single
+#' restaurant. The restaurant was part of a national chain
+#' and was located in a suburban shopping center.
+#'
+#' @format A data frame with 244 rows and 7 variables:
+#' \describe{
+#'   \item{total_bill}{total bill (cost of the meal), including tax, in US dollars}
+#'   \item{tip}{amount of gratuity received, in US dollars}
+#'   \item{sex}{sex of person paying (0 = male, 1 = female)}
+#'   \item{smoker}{was at least one member of the party a smoker? (0 = no, 1 = yes)}
+#'   \item{day}{3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday}
+#'   \item{time}{0 = day, 1 = night}
+#'   \item{size}{party size}
+#' }
+#' @source Bryant, P. G. and Smith, M. A. (1995), Practical Data Analysis: Case Studies
+#' in Business Statistics, Richard D. Irwin Publishing, Homewood, IL.
+"tips"
