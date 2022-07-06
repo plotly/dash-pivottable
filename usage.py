975973cb191b07bf762db4dc27dea58f9bab0cde
Adrian Issott <adrian.issott@hp.com>
2022-07-06 20:27:36 +0100

    Updating usage.py to demonstrate data being updated

--- usage.py
+++ usage.py
@@ -21,6 +21,7 @@ app.layout = html.Div([
         vals=["Total Bill"],
         valueFilter={'Day of Week': {'Thursday': False}}
     ),
+    html.Button('Toggle data', id='toggle-data-button', n_clicks=0),
     html.Div(
         id='output'
     )
@@ -33,8 +34,9 @@ app.layout = html.Div([
                Input('table', 'rowOrder'),
                Input('table', 'colOrder'),
                Input('table', 'aggregatorName'),
-               Input('table', 'rendererName')])
-def display_props(cols, rows, row_order, col_order, aggregator, renderer):
+               Input('table', 'rendererName'),
+               Input('table', 'data')])
+def display_props(cols, rows, row_order, col_order, aggregator, renderer, data_input):
     return [
         html.P(str(cols), id='columns'),
         html.P(str(rows), id='rows'),
@@ -42,7 +44,17 @@ def display_props(cols, rows, row_order, col_order, aggregator, renderer):
         html.P(str(col_order), id='col_order'),
         html.P(str(aggregator), id='aggregator'),
         html.P(str(renderer), id='renderer'),
+        html.P(f'Data length: {len(data_input)}', id='data_length'),
     ]
+
+
+@app.callback(Output('table', 'data'),
+              [Input('toggle-data-button', 'n_clicks')],
+              prevent_initial_call=True)
+def toggle_data(n_clicks):
+    if n_clicks % 2 == 0:
+        return data
+    return data[:len(data)//2]
 
 
 if __name__ == '__main__':
